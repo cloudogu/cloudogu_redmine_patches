674a7d29e9badf4912c9e9ac564c28b305866c7c
Dennis Schwarzer <dennis.schwarzer@cloudogu.com>
2025-08-26 16:07:16 +0200

    fixed right click context menu

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 Redmine::Plugin.register :zzz_cloudogu_redmine_patches do
   name        'Cloudogu Redmine Patches'
   author      'Dennis Schwarzer'
-  version     '0.0.9'
+  version     '0.0.10'
   description 'Upgrade Helper from Redmine 5 to Redmine 6. Fixes for example duplicate inline SVG icons (keep theme sprites), Active Session Store SLO Bug and view error regarding account/cas and restores legacy plugins compatibility'
 end
 
